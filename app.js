39c7999aab9929a56d5d525e115fb74768ae0ac1
Kumaravel187 <46121794+Kumaravel187@users.noreply.github.com>
2023-08-04 17:21:39 +0800

    Completed form validation to check if all fields are required

--- app.js
+++ app.js
@@ -18,11 +18,6 @@ function showSuccessMessage(input) {
   formControl.className = "form-control success";
 }
 
-function validateEmail(email) {
-  var re = /\S+@\S+\.\S+/;
-  return re.test(email);
-}
-
 function checkRequired(inputElArry) {
   inputElArry.forEach(function (inputEl) {
     if (inputEl.value.trim() === "") {
@@ -37,11 +32,44 @@ function getFieldName(inputEl) {
   return inputEl.id.charAt(0).toUpperCase() + inputEl.id.slice(1);
 }
 
-function showSuccess() {}
+function checkEmail(inputEl) {
+  const re = /\S+@\S+\.\S+/;
+  if (re.test(inputEl.value.trim())) {
+    showSuccessMessage();
+  } else {
+    showErrorMessage(inputEl, "Email is not valid");
+  }
+}
+
+function checkLength(inputEl, min, max) {
+  if (inputEl.value.length < min) {
+    showErrorMessage(
+      inputEl,
+      `${getFieldName(inputEl)} must be at least ${min} characters`
+    );
+  } else if (inputEl.value.length > max) {
+    showErrorMessage(
+      inputEl,
+      `${getFieldName(inputEl)} must be at most ${max} characters`
+    );
+  } else {
+    showSuccessMessage(inputEl);
+  }
+}
+
+function checkPassword(input1, input2) {
+  if (input1.value !== input2.value) {
+    showErrorMessage(input2, "Passwords do not match");
+  }
+}
 
 formEl.addEventListener("submit", function (e) {
   // prevent default form behaviour
   e.preventDefault();
 
   checkRequired([usernameEl, emailEl, passwordEl, passwordTwoEl]);
+  checkLength(usernameEl, 3, 20);
+  checkLength(passwordEl, 5, 25);
+  checkEmail(emailEl);
+  checkPassword(passwordEl, passwordTwoEl);
 });
